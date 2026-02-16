5258932af1e369adf9121e126b2df7a0c56ad1a0
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2026-02-17 00:19:23 +0300

    Update graph_unet.py

--- src/model/graph_unet.py
+++ src/model/graph_unet.py
@@ -8,6 +8,7 @@ from config import TOP_K, NUM_BLOCKS, EMBED_DIM
 class GraphUNet(nn.Module):
     def __init__(self, input_dim):
         super(GraphUNet, self).__init__()
+        # Initial embedding
         self.embedding = GCNLayer(input_dim, EMBED_DIM)
 
         # Encoder blocks
@@ -18,25 +19,26 @@ class GraphUNet(nn.Module):
         self.dec_unpools = nn.ModuleList([GUnpool() for _ in range(NUM_BLOCKS)])
         self.dec_gcns = nn.ModuleList([GCNLayer(EMBED_DIM, EMBED_DIM) for _ in range(NUM_BLOCKS)])
 
-        # Final GCN layer for output
+        # Final GCN for network embedding
         self.final_gcn = GCNLayer(EMBED_DIM, EMBED_DIM)
 
     def forward(self, X, A):
         X = self.embedding(X, A)
-        idx_list, A_list, X_list = [], [], []
-
-        # Encoder
+        skip_X, skip_A, idx_list = [], [], []
+        
         for gcn, pool in zip(self.enc_gcns, self.enc_pools):
             X = gcn(X, A)
             X, A, idx = pool(X, A)
+            skip_X.append(X)  
+            skip_A.append(A)
             idx_list.append(idx)
-            A_list.append(A)
-            X_list.append(X)
 
-        # Decoder
-        for gcn, unpool, idx in zip(self.dec_gcns, self.dec_unpools, reversed(idx_list)):
-            X = unpool(X, idx, N=A_list.pop(0).size(0))
-            X = gcn(X, A_list.pop(0))
+        for gcn, unpool, X_skip, idx, A_enc in zip(
+            self.dec_gcns, self.dec_unpools, reversed(skip_X), reversed(idx_list), reversed(skip_A)
+        ):
+            X = unpool(X, idx, N=A_enc.size(0))  
+            X = X + X_skip  
+            X = gcn(X, A_enc)
 
         X = self.final_gcn(X, A)
         return X
