ba4e26d957e7e9dc6cc882fe99ec53c98fea2d07
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2026-02-17 00:09:10 +0300

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,11 @@
+# Number of nodes to select in gPool layer
+TOP_K = 10
+
+# Number of encoder/decoder blocks
+NUM_BLOCKS = 2
+
+# Dimension of node embeddings
+EMBED_DIM = 16
+
+# Graph power for connectivity 
+GRAPH_POWER = 2
